205bb719f24dab748378a16a88bbcd143c2de064
jimbolol12 <97526093+jimbolol12@users.noreply.github.com>
2023-11-03 21:30:18 +0100

    inlog page done

---
+++ Barroc-intens/Barroc-intens/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Barroc_intens.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Barroc_intens.Model;
+
+namespace Barroc_intens
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;" +
+                "port=3306;" +
+                "user=root;" +
+                "database=Barroc-intens",
+                ServerVersion.Parse("8.0.30-mariadb")
+                );
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Username = "Alice",
+                    Password = "test",
+                }
+            );
+        }
+    }
+}

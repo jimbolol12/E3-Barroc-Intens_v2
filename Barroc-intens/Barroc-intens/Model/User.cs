f1a8013ddaef19585cfe6666f2ac9ce34abe016c
jimbolol12 <97526093+jimbolol12@users.noreply.github.com>
2023-11-03 11:14:13 +0100

    log in page added

---
+++ Barroc-intens/Barroc-intens/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaApp.Model
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public bool IsAdmin { get; set; }
+        public int? PickedUserId { get; set; }
+
+        public User(string username)
+        {
+            this.Username = username;
+            this.IsAdmin = false;
+            this.PickedUserId = null;
+        }
+    }
+}

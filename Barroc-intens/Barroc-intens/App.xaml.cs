3b00ffba315e82852081e8b783ed91f6104037dc
Luna <93603497+TheLunaWitch@users.noreply.github.com>
2023-12-05 12:00:41 +0100

    Update App.xaml.cs

--- Barroc-intens/Barroc-intens/App.xaml.cs
+++ Barroc-intens/Barroc-intens/App.xaml.cs
@@ -47,7 +47,7 @@ namespace Barroc_intens
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new KlantenWindow();
+            m_window = new LoginWindow();
             m_window.Activate();
         }
 
